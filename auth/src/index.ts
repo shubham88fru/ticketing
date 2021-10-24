60bb45f50cf053336f0f1e44d7a3fc0ac8ec6c2a
shubham88fru <shubham88fru@yahoo.com>
2021-10-24 17:34:00 +0530

    Setup all auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,12 +2,18 @@ import express from "express";
 import { json } from "body-parser";
 
 import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
 
 const app = express();
 
 // Middlewares
 app.use(json());
-app.use(currentUserRouter)
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
