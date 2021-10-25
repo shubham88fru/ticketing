58039380790c8579eb935105818060ca6b35ae53
shubham88fru <shubham88fru@yahoo.com>
2021-10-25 11:32:04 +0530

    Error handling middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handlers";
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+app.use(errorHandler);
 
 const PORT = 3000;
 app.listen(PORT, () => {
