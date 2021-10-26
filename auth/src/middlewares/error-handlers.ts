3c6fafb3f702450248480fda433023bdfdd5cea9
shubham88fru <shubham88fru@yahoo.com>
2021-10-26 11:00:25 +0530

    Refactor

--- auth/src/middlewares/error-handlers.ts
+++ auth/src/middlewares/error-handlers.ts
@@ -1,9 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const errorHandler = (err: Error, req: Request , res: Response, next: NextFunction) => {
-  console.log('"somthing went wrong err', err);
-
   res.status(400).send({
-    message: 'Something went wrong'
+    message: err.message
   });
 };
