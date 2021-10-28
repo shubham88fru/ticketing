80a9b794c25cfa407d3b79451e2e0277648a8ea8
shubham88fru <shubham88fru@yahoo.com>
2021-10-28 11:02:34 +0530

    Apply prettier formatting

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import { json } from "body-parser";
+import express from 'express';
+import { json } from 'body-parser';
 
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handlers";
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handlers';
 
 const app = express();
 
@@ -19,5 +19,5 @@ app.use(errorHandler);
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log("Listening on port 3000");
+  console.log('Listening on port 3000');
 });
--- auth/src/middlewares/error-handlers.ts
+++ auth/src/middlewares/error-handlers.ts
@@ -1,7 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
-export const errorHandler = (err: Error, req: Request , res: Response, next: NextFunction) => {
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
+    return res.status(400).send({ errors: formattedErrors });
+  }
+  if (err instanceof DatabaseConnectionError) console.log('Databse conn errr');
+
   res.status(400).send({
-    message: err.message
+    message: err.message,
   });
 };
