9e1b1873fde32e60defb475367253b18eed586fd
shubham88fru <shubham88fru@yahoo.com>
2021-10-31 11:22:43 +0530

    Add custom error for 404 routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
@@ -6,6 +7,7 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handlers';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 
@@ -15,6 +17,12 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+// Fallback for all non existent errors
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
 const PORT = 3000;
