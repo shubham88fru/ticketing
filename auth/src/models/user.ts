20ad1d69d128f211c825538010a30c51c1b40df5
shubham88fru <shubham88fru@yahoo.com>
2021-11-02 10:21:14 +0530

    Add interface for user document

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -9,8 +9,16 @@ interface UserAttrs {
 
 // describes the properties
 // that a user model has
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// Describes the  properties that
+// a User document has
+
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -29,6 +37,6 @@ userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-const User = mongoose.model<any, UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User };
