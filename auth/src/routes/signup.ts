92ee0cf982db713be5fc1b186ee3eca02ac92afb
shubham88fru <shubham88fru@yahoo.com>
2021-11-04 17:31:50 +0530

    Add env variable

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -39,7 +39,7 @@ router.post(
         id: user.id,
         email: user.email,
       },
-      'asdf'
+      process.env.JWT_KEY!
     );
 
     // Store it on session object (comming from cookie-session middleware)
