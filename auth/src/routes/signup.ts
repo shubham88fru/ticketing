0f108f0228506e80427dcda309f82bbb5ea0cd03
shubham88fru <shubham88fru@yahoo.com>
2021-11-04 17:13:03 +0530

    Add jwt as a cookie in server response

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -30,6 +32,20 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate jwt
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      'asdf'
+    );
+
+    // Store it on session object (comming from cookie-session middleware)
+    req.session = {
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   }
