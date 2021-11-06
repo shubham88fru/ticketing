232d6e8b08c419034663296dddc049e5ffeeaf5e
Shubham Singh <shubham88fru@yahoo.com>
2021-11-07 01:54:03 +0530

    Add more tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,39 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns a 400 for an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtesty.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns a 400 for an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtesty.com',
+      password: 'p',
+    })
+    .expect(400);
+});
+
+it('returns a 400 for missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtesty.com',
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'password',
+    })
+    .expect(400);
+});
