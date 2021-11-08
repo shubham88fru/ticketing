5fc15ba5b280101bfad5fc4b285aad86f4240b0f
shubham88fru <shubham88fru@yahoo.com>
2021-11-08 09:27:21 +0530

    implement get initial props

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,12 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
   return <h1>Index page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get('/api/users/currentuser');
+  return response.data;
+};
+
+export default LandingPage;
