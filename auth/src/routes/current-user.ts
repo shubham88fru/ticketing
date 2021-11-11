73eab4552ec5a9018790c550076ed914fc2c5d2d
shubham88fru <shubham88fru@yahoo.com>
2021-11-11 11:47:33 +0530

    fix imports

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@wolvtickets/common';
 
 const router = express.Router();
 
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -4,8 +4,7 @@ import jwt from 'jsonwebtoken';
 
 import { User } from '../models/user';
 import { Password } from '../services/password';
-import { validateRequest } from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest, BadRequestError } from '@wolvtickets/common';
 
 const router = express.Router();
 
