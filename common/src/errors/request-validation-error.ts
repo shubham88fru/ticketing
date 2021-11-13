0e61064646c9286d9ec802c9ffa0e611a894bb0f
shubham88fru <shubham88fru@yahoo.com>
2021-11-13 17:33:38 +0530

    more code for new ticket route

--- common/src/errors/request-validation-error.ts
+++ common/src/errors/request-validation-error.ts
@@ -5,7 +5,7 @@ export class RequestValidationError extends CustomError {
   statusCode = 400;
   constructor(public errors: ValidationError[]) {
     super('Invalid request parameters');
-    // Only because we are extending a build in class
+    // Only because we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
 
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -18,6 +18,42 @@ it('returns a status other than 401 if the user is signedin', async () => {
   expect(response.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid price is provided', async () => {});
+it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10,
+    })
+    .expect(400);
+});
+
+it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'sdfghjk',
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'dfghjk',
+    })
+    .expect(400);
+});
 
 it('creates a ticket with valid inputs', async () => {});
