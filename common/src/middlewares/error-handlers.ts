f224a7e4f67125ebeb437ff7404ee2a30460f56a
shubham88fru <shubham88fru@yahoo.com>
2021-11-14 11:40:06 +0530

    Updates

--- common/src/middlewares/error-handlers.ts
+++ common/src/middlewares/error-handlers.ts
@@ -11,6 +11,7 @@ export const errorHandler = (
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
+  console.error('Some un-expected error occurred', err);
   res.status(400).send({
     errors: [
       {
