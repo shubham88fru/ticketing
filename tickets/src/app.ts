6fa21dfaa37cb350692f92e3af6ebdc91b133cbb
shubham88fru <shubham88fru@yahoo.com>
2021-11-14 11:52:47 +0530

    list all tickets route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,6 +6,7 @@ import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@wolvtickets/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
 
 const app = express();
 app.set('trust proxy', true); // Trust https connection being proxies from ingress
@@ -18,6 +19,7 @@ app.use(
 app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 // Fallback for all non existent errors
 app.all('*', async (req, res) => {
