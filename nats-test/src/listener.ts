c888a216eed5a76cc6acefe8dd3fa5161d788e51
shubham88fru <shubham88fru@yahoo.com>
2021-11-27 11:46:12 +0530

    more code for publisher and listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,16 +1,28 @@
-import nats from 'node-nats-streaming';
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created');
-  subscription.on('message', (msg) => {
-    console.log('Message received');
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'listener-service-queue-group',
+    options
+  );
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+
+    msg.ack();
   });
 });
