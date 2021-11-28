635b99b6e025618c26d29ca9493d736a626a6507
shubham88fru <shubham88fru@yahoo.com>
2021-11-28 17:54:23 +0530

    close nats client and mark it dead

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,6 +10,11 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
+  stan.on('close', () => {
+    console.log('Nats connection closed');
+    process.exit();
+  });
+
   const options = stan.subscriptionOptions().setManualAckMode(true);
   const subscription = stan.subscribe(
     'ticket:created',
@@ -26,3 +31,6 @@ stan.on('connect', () => {
     msg.ack();
   });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
