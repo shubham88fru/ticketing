77aa02993b1d214e2249bace3c790714785b2f43
shubham88fru <shubham88fru@yahoo.com>
2021-12-26 10:48:50 +0530

    publish events on new order or cancelling an order

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,6 +5,8 @@ import {
   requireAuth,
 } from '@wolvtickets/common';
 import { Order, OrderStatus } from '../models/order';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -13,7 +15,7 @@ router.delete(
   requireAuth,
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket');
 
     if (!order) {
       throw new NotFoundError();
@@ -25,6 +27,12 @@ router.delete(
     await order.save();
 
     //publish an event saying this was cancelled.
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
 
     res.status(204).send(order);
   }
