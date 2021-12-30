f7bf53f54b2e2a7f708489efa4e0d847dabfd99d
shubham88fru <shubham88fru@yahoo.com>
2021-12-30 11:07:55 +0530

    add order created listenerr

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,9 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@wolvtickets/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName: string = queueGroupName;
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+}
