d0aacc5af8a86efe8330767014f5a784fea28128
shubham88fru <shubham88fru@yahoo.com>
2021-12-30 10:52:54 +0530

    fix failing tests due to missing id field

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -3,10 +3,12 @@ import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
 import { Order, OrderStatus } from '../../models/order';
 import { natsWrapper } from '../../nats-wrapper';
+import mongoose from 'mongoose';
 
 it('marks an order as cancelled', async () => {
   // create a ticket with ticket model.
   const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
     title: 'concert',
     price: 20,
   });
@@ -36,6 +38,7 @@ it('marks an order as cancelled', async () => {
 it('Emits an event for the order canclled', async () => {
   // create a ticket with ticket model.
   const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
     title: 'concert',
     price: 20,
   });
